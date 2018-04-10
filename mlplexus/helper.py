deac2ae2a6c09817edba82065c17fb0616facb4f
Ankit Khambhati <akhambhati@gmail.com>
2018-04-09 22:11:08 -0700

    Function to get class name as a string from object

---
+++ mlplexus/helper.py
@@ -0,0 +1,14 @@
+"""
+Define helper functions for mlPlexus.
+
+Author: Ankit N. Khambhati
+Created: 2018/04/09
+"""
+
+
+def class_as_string(obj):
+    """Return the full class of an object as string"""
+    module = obj.__class__.__module__
+    if module is None or module == str.__class__.__module__:
+        return obj.__class__.__name__
+    return module + '.' + obj.__class__.__name__
