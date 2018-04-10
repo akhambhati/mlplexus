1a8cb864d4367ef371b365b76f3c424ff2f40c6a
Ankit Khambhati <akhambhati@gmail.com>
2018-04-09 22:10:42 -0700

    Significant revision of the GraphArch class

--- mlplexus/graph.py
+++ mlplexus/graph.py
@@ -1,45 +1,36 @@
 """
 Implements the basic infrastructure of
-Nodes, BaseGraph, MonoGraph, and MultiGraph.
+Graph, GraphState.
 
 Author: Ankit N. Khambhati
 Created: 2018/02/28
-Updated: 2018/02/28
+Updated: 2018/04/09
 """
 
 import numpy as np
 
-from mlplexus.checks import (checkArrDims, checkArrDTypeStr, checkArrSqr,
-                             checkNone, checkType)
-from mlplexus.exception import (mlPlexusException, mlPlexusNotImplemented,
-                                mlPlexusTypeError, mlPlexusValueError)
+from mlplexus.checks import (checkArrDims, checkArrDTypeStr, checkArrLen,
+                             checkArrSqr, checkNone, checkPath, checkType)
+from mlplexus.exception import (mlPlexusException, mlPlexusIOError,
+                                mlPlexusNotImplemented, mlPlexusTypeError,
+                                mlPlexusValueError)
+from mlplexus.helper import class_as_string
 
 
-class Nodes(object):
+class GraphArch(object):
     """
-    Base class to establish a collection of Nodes.
+    Class to define the immutable properties of the Graph Architecture.
 
-    Nodes is a collection of individual nodes, each with an
+    GraphArch is a collection of individual nodes, each with an
     immutable identity in the system.
 
-    *NOTE*: Any mutable attributes of Nodes are identified in objects
-    derived from the Graph class, _not_ in the Nodes class.
-    For example:
-        For an instance of Nodes defining a collection of
-        humans, dogs, and cats might have a common attribute type
-        called `Animal`, which would reasonably remain immutable
-        regardless of when/where/how the nodes are observed.
-
-        Alternatively, for an instance of Nodes defining a collection of
-        humans with a common attribute type called `Location', might be
-        immutable if we expect humans to remain at the same spot in the system.
-        If we do not assume they will stay in the same spot in the system,
-        then `Location' should not be defined in the Nodes instance.
+    *NOTE*: Any mutable attributes of the Graph are identified in objects
+    derived from the GraphState class, _not_ in the Graph class.
     """
 
     def __init__(self, node_id=None, **node_attr):
         """
-        Initialize Nodes with specific ID and set attributes
+        Initialize graph with nodes of specific ID and attributes.
 
         Parameters
         ----------
@@ -47,14 +38,8 @@ class Nodes(object):
                 List of strings, where each string identifies a node.
 
             node_attr: key/value -->
-                         value=None             OR
-                         value=obj              OR
-                         value=list(obj,)
+                         value=np.ndarray(obj,)
                 Each attribute is defined as a key-value pair.
-                    Use `key=None` when defining key as a placeholder.
-                    Use `key=obj` when defining same key-value across nodes.
-                    Use `key=list(obj,)` when defining different
-                        key-value across nodes.
         """
 
         # First, handle the node_id
@@ -67,156 +52,118 @@ class Nodes(object):
         if not checkArrDTypeStr(node_id):
             raise mlPlexusTypeError(
                 'Must supply a node_id containing strings as identifiers')
-        self._id = node_id
-        self.n_node = len(self._id)
+        n_node = len(node_id)
 
         # Second, handle the incoming attributes
         if not checkType(node_attr, dict):
             raise mlPlexusTypeError('All attributes must be supplied in'
                                     ' conventional Python `dict` format')
 
-        # Check how number of values compares to number of nodes
+        # Construct a list of numpy dtypes for the structure
+        dtype_tuple = []
         for key in [*node_attr]:
-            if checkType(node_attr[key], np.ndarray):
-                node_attr[key] = list(node_attr[key])
-            if checkType(node_attr[key], list):
-                if len(node_attr[key]) != self.n_node:
-                    raise mlPlexusTypeError('Must supply same number of values'
-                                            ' as nodes for attribute'
-                                            ' \'{}\'.'.format(key))
-            else:
-                node_attr[key] = [node_attr[key] for i in range(self.n_node)]
+            key_attr = node_attr[key].squeeze()
+            if not checkType(key_attr, np.ndarray):
+                raise mlPlexusTypeError('Must supply numpy array for attribute'
+                                        ' \'{}\'.'.format(key))
+            if not checkArrLen(key_attr, n_node):
+                raise mlPlexusTypeError('Must supply numpy array with'
+                                        ' same number of values as nodes'
+                                        ' in first dimension, for attribute'
+                                        ' \'{}\'.'.format(key))
+            dtype_tuple.append((key, key_attr.dtype, key_attr.shape[1:]))
+        dtype_tuple.append(('node_id', node_id.dtype, node_id.shape[1:]))
 
-        # Passed checks for properly formatted value
-        self._node_attr = node_attr
+        # Generate a Structured Array from the attributes
+        arr_node_attr = np.empty(n_node, dtype=dtype_tuple)
+        arr_node_attr['node_id'] = node_id
+        for key in [*node_attr]:
+            arr_node_attr[key] = node_attr[key]
+
+        # Set attributes for the class attributes
+        self._define_graph(arr_node_attr)
+
+    def _define_graph(self, node_attr):
+        """Define class instance attributes from node attribute array"""
+        self.node_attr = node_attr
+        self.node_id = node_attr['node_id']
+        self.n_node = len(node_attr)
+        self.node_attr.flags.writeable = False
+
+    def get_node(self, node):
+        """Return the attributes for a node"""
+        node_ix = self.node_attr['node_id'] == node
+        return self.node_attr[node_ix]
 
     def __str__(self):
         """Prettified string representation of node ids."""
         s = '\n::NODES::\n'
-        return s + '\n'.join(self._id)
+        return s + '\n'.join(self.node_id)
 
-    def get_id(self):
-        """Return the node IDs."""
-        return self._id
-
-    def get_attr_key(self):
-        """Return keys defining attributes for the nodes."""
-        return [*self._node_attr]
-
-    def get_attr_val(self, key=None):
-        """Return node attribute values corresponding to a key."""
-        if checkNone(key):
-            raise mlPlexusException('A node attribute key was not entered.')
-        if checkType(key, list) or checkType(key, np.ndarray):
-            raise mlPlexusTypeError('Cannot supply keys as array-like lists.')
-        if key in [*self._node_attr]:
-            return self._node_attr[key]
-        else:
-            raise mlPlexusTypeError('Key {} is not a valid attribute.'
-                                    .format(key))
-
-
-class Graph(object):
-    """
-    Base class to establish the Graph object; implementation
-    is pure python, numpy.
-
-    Graph provides a flexible format define the
-    topology of edges between the nodes.
-
-    Graph defines the topology of edges between the nodes as a function
-    for an arbitrary, user-defined complex system state.
-
-    Constraints within the Graph object:
-        1) Nodes are predefined in an instance of Nodes class.
-        2) Two nodes can only have one edge between them.
-        3) Mode of edge interaction is completely described
-           by the state-definition. Edge type is not explicitly defined.
-    """
-
-    def __init__(self, nodes=None, A=None, **graph_state):
+    def save(self, graph_name, graph_dir):
         """
-        Initialize Graph with an instance of Nodes, a specification of
-        edges interlinking the nodes in Nodes, and a state definition.
+        Save the constructed Graph Architecture.
+
+        The Graph Architecture can be fully reconstructed from the attribute,
+        node_attr.
 
         Parameters
         ----------
-            nodes: mlplexus.graph.Nodes
-                An instance of defined nodes with node attributes.
-            A: np.ndarray, shape: (n_node, n_node)
-                Adjacency matrix that defines the interlinks between nodes.
-                Matrix should a size equal in size to the number of nodes.
-            graph_state: key/value -->
-                         value=None             OR
-                         value=obj
-                Each state attribute is defined as a key-value pair.
-                    Use `key=None` when defining key as a placeholder.
-                    Use `key=obj` when defining key with any hashable object.
+            graph_name: str
+                Unique name given to the defined Graph Architecture.
+            graph_dir: str
+                The directory in which the Graph Architecture will be saved.
+
+        Returns
+        -------
+            graph_path: str
+                Saved file will have the path,
+                <graph_dir>/mlplexus.graph.GraphArch.<graph_name>.npy
         """
 
-        # First, handle nodes
-        if checkNone(nodes) or (not checkType(nodes, Nodes)):
-            raise mlPlexusTypeError('Must supply an instance of'
-                                    ' mlplexus.graph.Nodes')
-        self.nodes = nodes
+        # First, check the inputs
+        if (not checkType(graph_name, str)) or (not checkType(graph_dir, str)):
+            raise mlPlexusTypeError('Must supply name and directory as str.')
 
-        # Second, handle A
-        if not checkType(A, np.ndarray):
-            raise mlPlexusTypeError('Must supply an Adjacency matrix'
-                                    ' as a square, numpy.ndarray.')
-        if not checkArrDims(A, 2):
-            raise mlPlexusException('Must supply an Adjacency matrix'
-                                    ' with square, 2d-array.')
-        if not checkArrSqr(A):
-            raise mlPlexusException('Must supply an Adjacency matrix'
-                                    ' with square, 2d-array.')
-        if not len(A) == self.nodes.n_node:
-            raise mlPlexusException('Must supply an Adjacency matrix'
-                                    ' with size equal to nodes.')
-        if np.isnan(A).any():
-            raise mlPlexusValueError('Please resolve NaN in Adjacency matrix.')
+        # Get the class string
+        cstr = class_as_string(self)
 
-        # Infer graph properties from structure of adjacency matrix
-        self.graph_props = {
-            'directed': not np.array_equal(A, A.T),
-            'binarized': np.array_equal(A, A.astype(bool)),
-            'loops': np.diag(A > 0).any()
-        }
-        self._A_to_Ahat(A)
+        # Construct the full path
+        graph_path = '{}/{}.{}.npy'.format(graph_dir, cstr, graph_name)
 
-        # Third, handle the incoming state-definition
-        if not checkType(graph_state, dict):
-            raise mlPlexusTypeError('All state-defs must be supplied in'
-                                    ' conventional Python `dict` format')
-        self._graph_state = graph_state
+        if checkPath(graph_path):
+            raise mlPlexusIOError(
+                'The path \'{}\' already exists!'.format(graph_path))
+        np.save(graph_path, self.node_attr)
 
-    def _A_to_Ahat(self, A):
-        """Use graph properties to convert adj. matr. to cfg. vec"""
-        if hasattr(self, '_Ahat'):
-            return 0
+        return graph_path
 
-        ix, iy = np.triu_indices_from(A, k=1)
-        if self.graph_props['directed']:
-            tril = np.tril_indices_from(A, k=-1)
-            ix = np.concatenate((ix, tril[0]))
-            iy = np.concatenate((iy, tril[1]))
-        if self.graph_props['loops']:
-            diag = np.diag_indices_from(A)
-            ix = np.concatenate((ix, diag[0]))
-            iy = np.concatenate((iy, diag[1]))
-        self._trans_ix = ix
-        self._trans_iy = iy
+    def load(self, graph_path):
+        """
+        Load the Graph Architecture.
 
-        self.Ahat = A[self._trans_ix, self._trans_iy]
+        The Graph Architecture can be fully reconstructed from the attribute,
+        node_attr.
 
-    def get_adjacency_matrix(self):
-        """Return the matrix representation of the Graph."""
-        A = np.zeros((self.nodes.n_node, self.nodes.n_node))
-        A[self._trans_ix, self._trans_iy] = self.Ahat
-        if not self.graph_props['directed']:
-            A += np.triu(A, k=1).T
-        return A
+        Parameters
+        ----------
+            graph_path: str
+                <graph_dir>/mlplexus.graph.GraphArch.<graph_name>.npy
+        """
 
-    def get_configuration_vector(self):
-        """Return the vector representation of the Graph."""
-        return self.Ahat
+        # First, check the inputs
+        if (not checkType(graph_path, str)):
+            raise mlPlexusTypeError('Must supply path as str.')
+        if not checkPath(graph_path):
+            raise mlPlexusIOError(
+                'The path \'{}\' does not exists.'.format(graph_path))
+
+        # Get the class string
+        cstr = class_as_string(self)
+
+        if cstr not in graph_path:
+            raise mlPlexusIOError('The file \'{}\' is invalid for'
+                                  ' class \'{}\'.'.format(graph_path, cstr))
+
+        # Re-define the class instance
+        self._define_graph(np.load(graph_path, mmap_mode='r'))
