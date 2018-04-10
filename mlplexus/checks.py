5f2cfeb4003454e941c52f72f51c7705b355fc04
Ankit Khambhati <akhambhati@gmail.com>
2018-04-09 22:09:51 -0700

    Added array length checking and path checking

--- mlplexus/checks.py
+++ mlplexus/checks.py
@@ -6,6 +6,8 @@ Created: 2018/02/28
 Updated: 2018/02/28
 """
 
+import os
+
 import numpy as np
 
 
@@ -20,6 +22,14 @@ def checkArrSqr(arr):
 def checkArrDims(arr, n_dim):
     """Check if arr has n_dim dimensions"""
     if arr.ndim == n_dim:
+        return True
+    else:
+        return False
+
+
+def checkArrLen(arr, length):
+    """Check if arr has length of length"""
+    if len(arr) == length:
         return True
     else:
         return False
@@ -47,3 +57,8 @@ def checkType(obj, ref_type):
         return True
     else:
         return False
+
+
+def checkPath(path):
+    """Check if path exists"""
+    return os.path.exists(path)
