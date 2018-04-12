97d9c74c5a3860819956009334d58e43104500dc
Ankit Khambhati <akhambhati@gmail.com>
2018-04-11 19:41:57 -0700

    Added a verbose print function

--- mlplexus/helper.py
+++ mlplexus/helper.py
@@ -12,3 +12,7 @@ def class_as_string(obj):
     if module is None or module == str.__class__.__module__:
         return obj.__class__.__name__
     return module + '.' + obj.__class__.__name__
+
+def vprint(obj, verbose=True, **kwargs):
+    if verbose:
+        print(obj, **kwargs)
